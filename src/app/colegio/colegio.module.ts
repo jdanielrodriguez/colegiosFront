df89ea9ad36e5944c342ba242c484c35ae0ac273
Daniel Rodriguez <daniel@foxylabs.gt>
2017-08-29 12:35:10 -0600

    [UPDATED] added toures for students, teachers and tutors

--- src/app/colegio/colegio.module.ts
+++ src/app/colegio/colegio.module.ts
@@ -4,11 +4,20 @@ import { CommonModule } from '@angular/common';
 import { ColegioRoutingModule } from './colegio.routing';
 import { NavComponent } from './nav.component';
 
+import { StudentGuard } from "./../_guards/student.guard";
+import { TutorGuard } from "./../_guards/tutor.guard";
+import { TeacherGuard } from "./../_guards/teacher.guard";
+
 @NgModule({
   imports: [
     CommonModule,
     ColegioRoutingModule
   ],
-  declarations: [NavComponent]
+  declarations: [NavComponent],
+  providers: [
+    StudentGuard,
+    TutorGuard,
+    TeacherGuard
+  ]
 })
 export class ColegioModule { }
--- src/app/colegio/colegio.routing.ts
+++ src/app/colegio/colegio.routing.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { NavComponent } from "./nav.component";
+import { StudentGuard } from "./../_guards/student.guard";
+import { TutorGuard } from "./../_guards/tutor.guard";
+import { TeacherGuard } from "./../_guards/teacher.guard";
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'estudiante', pathMatch: 'full' },
+  { path: '', component: NavComponent, children: [
+    { path: 'estudiante',loadChildren: 'app/colegio/estudiante/estudiante.module#EstudianteModule', canActivate: [StudentGuard]},
+    { path: 'tutor',loadChildren: 'app/colegio/tutor/tutor.module#TutorModule', canActivate: [TutorGuard]},
+    { path: 'maestro',loadChildren: 'app/colegio/maestro/maestro.module#MaestroModule', canActivate: [TeacherGuard]},
+  ]}
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
