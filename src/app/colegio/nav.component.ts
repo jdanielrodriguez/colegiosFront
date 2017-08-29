93a4f67fcdc34049a6ba6ab111973bb5697358dd
Daniel Rodriguez <daniel@foxylabs.gt>
2017-08-29 12:34:46 -0600

    [UPDATED] added html to nav component

--- src/app/colegio/nav.component.ts
+++ src/app/colegio/nav.component.ts
@@ -6,10 +6,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./nav.component.css']
 })
 export class NavComponent implements OnInit {
-
+  user=localStorage.getItem('currentUser');
+  firstname=localStorage.getItem('currentFirstName');
+  lastname=localStorage.getItem('currentLastName');
+  id=localStorage.getItem('currentId');
+  type=localStorage.getItem('currentType');
   constructor() { }
 
   ngOnInit() {
   }
-
+  logout() {
+    // remove user from local storage to log user out
+    localStorage.removeItem('currentUser');
+    localStorage.removeItem('currentEmail');
+    localStorage.removeItem('currentFirstName');
+    localStorage.removeItem('currentLastName');
+    localStorage.removeItem('currentId');
+    localStorage.removeItem('currentType');
+    location.reload();
+  }
 }
