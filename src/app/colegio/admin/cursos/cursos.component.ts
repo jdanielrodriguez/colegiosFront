b35c6cd3177f9e3aa4b8807c7fa59bc582e4143c
Daniel Rodriguez <daniel@foxylabs.gt>
2017-08-31 16:35:19 -0600

    [UPDATED] added funciton for subjects display

--- src/app/colegio/admin/cursos/cursos.component.ts
+++ src/app/colegio/admin/cursos/cursos.component.ts
@@ -1,15 +1,106 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute } from "@angular/router";
 
+import { SubjectsService } from "../_services/subjects.service";
+import { NotificationsService } from 'angular2-notifications';
 @Component({
   selector: 'app-cursos',
   templateUrl: './cursos.component.html',
   styleUrls: ['./cursos.component.css']
 })
 export class CursosComponent implements OnInit {
+  Table:any
+  selectedData:any
+  constructor(
+    private _service: NotificationsService,
+    private route: ActivatedRoute,
+    private router: Router,
+    private mainService: SubjectsService
+  ) { }
+  
+    ngOnInit() {
+      this.cargarAll()
+    }
+    cargarAll(){
+      this.mainService.getAll()
+                        .then(response => {
+                          this.Table = response
+                          $("#editModal .close").click();
+                          $("#insertModal .close").click();
+                          console.clear 
+                        }).catch(error => {
+                          console.clear     
+                          this.createError(error) 
+                        })
+    }
+    cargarSingle(id:number){
+      this.mainService.getSingle(id)
+                        .then(response => {
+                          this.selectedData = response;
+                        }).catch(error => {
+                          console.clear     
+                          this.createError(error) 
+                        })
+    }
+    update(formValue:any){
+      
+      //console.log(data)
+      this.mainService.update(formValue)
+                        .then(response => {
+                          this.cargarAll()
+                          console.clear 
+                          this.create('Curso Actualizado exitosamente')
+                        }).catch(error => {
+                          console.clear     
+                          this.createError(error) 
+                        })
+      
+    }
+    delete(id:string){
+      this.mainService.delete(id)
+                        .then(response => {
+                          this.cargarAll()
+                          console.clear 
+                          this.create('Curso Eliminado exitosamente')
+                        }).catch(error => {
+                          console.clear     
+                          this.createError(error) 
+                        })
+      
+    }
+    insert(formValue:any){
+      
+      this.mainService.create(formValue)
+                        .then(response => {
+                          this.cargarAll()
+                          console.clear 
+                          this.create('Curso Ingresado')
+                          
+                        }).catch(error => {
+                          console.clear     
+                          this.createError(error) 
+                        })
+      
+      
+    }
+    
+  public options = {
+               position: ["bottom", "right"],
+               timeOut: 2000,
+               lastOnBottom: false,
+               animate: "fromLeft",  
+               showProgressBar: false,
+               pauseOnHover: true,
+               clickToClose: true,
+               maxLength: 200
+           };
+  
+    create(success) {
+                this._service.success('¡Éxito!',success)
 
-  constructor() { }
+    }
+    createError(error) {
+                this._service.error('¡Error!',error)
 
-  ngOnInit() {
-  }
-
+    }
 }
