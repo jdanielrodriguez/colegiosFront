6d103f4a6790b02a5854e76b720a244cd78e79a1
Daniel Rodriguez <daniel@foxylabs.gt>
2017-09-12 15:36:57 -0600

    [ADDED] [src/app/colegio/admin/eventos-tipo/, src/app/colegio/admin/_services/events-type.service.spec.ts] added events types component

---
+++ src/app/colegio/admin/eventos-tipo/eventos-tipo.component.ts
@@ -0,0 +1,114 @@
+import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute } from "@angular/router";
+
+import { EventsTypeService } from "../_services/events-type.service";
+import { NotificationsService } from 'angular2-notifications';
+
+@Component({
+  selector: 'app-eventos-tipo',
+  templateUrl: './eventos-tipo.component.html',
+  styleUrls: ['./eventos-tipo.component.css']
+})
+export class EventosTipoComponent implements OnInit {
+  Table:any
+  selectedData:any
+  constructor(
+    private _service: NotificationsService,
+    private route: ActivatedRoute,
+    private router: Router,
+    private mainService: EventsTypeService
+  ) { }
+  
+    ngOnInit() {
+      this.cargarAll()
+    }
+    cargarAll(){
+      this.mainService.getAll()
+                        .then(response => {
+                          this.Table = response
+                          $("#editModal .close").click();
+                          $("#insertModal .close").click();
+                          console.clear 
+                        }).catch(error => {
+                          console.clear     
+                          this.createError(error) 
+                        })
+    }
+    cargarSingle(id:number){
+      this.mainService.getSingle(id)
+                        .then(response => {
+                          this.selectedData = response;
+                        }).catch(error => {
+                          console.clear     
+                          this.createError(error) 
+                        })
+    }
+    update(formValue:any){
+      $('#Loading').css('display','block')
+      $('#Loading').addClass('in')
+      //console.log(data)
+      this.mainService.update(formValue)
+                        .then(response => {
+                          this.cargarAll()
+                          console.clear 
+                          this.create('Tipo de Usuario Actualizado exitosamente')
+                          $('#Loading').css('display','none')
+                        }).catch(error => {
+                          console.clear     
+                          this.createError(error) 
+                        })
+      
+    }
+    delete(id:string){
+      $('#Loading').css('display','block')
+      $('#Loading').addClass('in')
+      this.mainService.delete(id)
+                        .then(response => {
+                          this.cargarAll()
+                          console.clear 
+                          this.create('Tipo de Usuario Eliminado exitosamente')
+                          $('#Loading').css('display','none')
+                        }).catch(error => {
+                          console.clear     
+                          this.createError(error) 
+                        })
+      
+    }
+    insert(formValue:any){
+      $('#Loading').css('display','block')
+      $('#Loading').addClass('in')
+      this.mainService.create(formValue)
+                        .then(response => {
+                          this.cargarAll()
+                          console.clear 
+                          this.create('Tipo de Usuario Ingresado')
+                          $('#Loading').css('display','none')
+                          
+                        }).catch(error => {
+                          console.clear     
+                          this.createError(error) 
+                        })
+      
+      
+    }
+    
+  public options = {
+               position: ["bottom", "right"],
+               timeOut: 2000,
+               lastOnBottom: false,
+               animate: "fromLeft",  
+               showProgressBar: false,
+               pauseOnHover: true,
+               clickToClose: true,
+               maxLength: 200
+           };
+  
+    create(success) {
+                this._service.success('¡Éxito!',success)
+
+    }
+    createError(error) {
+                this._service.error('¡Error!',error)
+
+    }
+}
