beeca749baf6e3cdb044d1754a2c23f6e132ec37
José Daniel <daniel@foxylabs.gt>
2017-09-15 19:59:56 -0600

    [ADDED] added charges component

---
+++ src/app/colegio/admin/cargos/cargos.component.ts
@@ -0,0 +1,137 @@
+import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute } from "@angular/router";
+
+import { StudentsService } from "../_services/students.service";
+import { NotificationsService } from 'angular2-notifications';
+import { Subject } from 'rxjs/Rx';
+@Component({
+  selector: 'app-cargos',
+  templateUrl: './cargos.component.html',
+  styleUrls: ['./cargos.component.css']
+})
+export class CargosComponent implements OnInit {
+  Table:any
+  selectedData:any
+  dtOptions: DataTables.Settings = {}
+  dtTrigger: Subject<any> = new Subject<any>();
+  dtTriggerDetail: Subject<any> = new Subject<any>();
+  constructor(
+    private _service: NotificationsService,
+    private route: ActivatedRoute,
+    private router: Router,
+    private mainService: StudentsService
+  ) { }
+  
+    ngOnInit() {
+      this.dtOptions = {
+        pagingType: 'full_numbers',
+        language: {
+          emptyTable: 'Tabla limpia',
+          info: 'Mostrando página _PAGE_ de _PAGES_',
+          infoEmpty: 'No hay registros disponibles',
+          infoFiltered: '(filtrado de _MAX_ registros totales)',
+          zeroRecords: 'Nada para mostrar, lo sentimos',
+          search: 'Buscar',
+          lengthMenu: 'Mostranto _MENU_ registro por página',
+          paginate: {
+            first: 'Primero',
+            last: 'Ultimo',
+            next: 'Siguiente',
+            previous: 'Anterior'
+          }
+  
+        }
+      };
+      this.cargarAll()
+    }
+    cargarAll(){
+      this.mainService.getAll()
+                        .then(response => {
+                          this.Table = response
+                          this.dtTrigger.next()                          
+                          $("#editModal .close").click();
+                          $("#insertModal .close").click();
+                          console.clear 
+                        }).catch(error => {
+                          console.clear     
+                          this.createError(error) 
+                        })
+    }
+    cargarSingle(id:number){
+      this.mainService.getSingle(id)
+                        .then(response => {
+                          this.selectedData = response;
+                        }).catch(error => {
+                          console.clear     
+                          this.createError(error) 
+                        })
+    }
+    update(formValue:any){
+      $('#Loading').css('display','block')
+      $('#Loading').addClass('in')
+      //console.log(data)
+      this.mainService.update(formValue)
+                        .then(response => {
+                          this.cargarAll()
+                          console.clear 
+                          this.create('Estudiante Actualizado exitosamente')
+                          $('#Loading').css('display','none')
+                        }).catch(error => {
+                          console.clear     
+                          this.createError(error) 
+                        })
+      
+    }
+    delete(id:string){
+      $('#Loading').css('display','block')
+      $('#Loading').addClass('in')
+      this.mainService.delete(id)
+                        .then(response => {
+                          this.cargarAll()
+                          console.clear 
+                          this.create('Estudiante Eliminado exitosamente')
+                          $('#Loading').css('display','none')
+                        }).catch(error => {
+                          console.clear     
+                          this.createError(error) 
+                        })
+      
+    }
+    insert(formValue:any){
+      $('#Loading').css('display','block')
+      $('#Loading').addClass('in')
+      this.mainService.create(formValue)
+                        .then(response => {
+                          this.cargarAll()
+                          console.clear 
+                          this.create('Estudiante Ingresado')
+                          $('#Loading').css('display','none')
+                          
+                        }).catch(error => {
+                          console.clear     
+                          this.createError(error) 
+                        })
+      
+      
+    }
+    
+  public options = {
+               position: ["bottom", "right"],
+               timeOut: 2000,
+               lastOnBottom: false,
+               animate: "fromLeft",  
+               showProgressBar: false,
+               pauseOnHover: true,
+               clickToClose: true,
+               maxLength: 200
+           };
+  
+    create(success) {
+                this._service.success('¡Éxito!',success)
+
+    }
+    createError(error) {
+                this._service.error('¡Error!',error)
+
+    }
+}
