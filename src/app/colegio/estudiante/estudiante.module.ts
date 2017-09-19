6c4da77a41f8f27f38bdb96b273472d8d3038f18
Daniel Rodriguez <daniel@foxylabs.gt>
2017-09-19 16:35:44 -0600

    [UPDATED] added basic configuration to students module

--- src/app/colegio/estudiante/estudiante.module.ts
+++ src/app/colegio/estudiante/estudiante.module.ts
@@ -1,12 +1,26 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from "@angular/forms";
+import { DataTablesModule } from 'angular-datatables';
 
 import { EstudianteRoutingModule } from './estudiante.routing';
 import { EstudianteComponent } from './estudiante.component';
 
+import { SimpleNotificationsModule } from 'angular2-notifications';
+import { ChartsModule } from 'ng2-charts';
+import { Ng2SearchPipeModule } from 'ng2-search-filter';
+import { Ng2DragDropModule } from 'ng2-drag-drop';
+import { LoadersCssModule } from 'angular2-loaders-css';
 @NgModule({
   imports: [
     CommonModule,
+    FormsModule,
+    DataTablesModule, 
+    ChartsModule,
+    SimpleNotificationsModule.forRoot(),  
+    Ng2SearchPipeModule, 
+    Ng2DragDropModule.forRoot(),
+    LoadersCssModule, 
     EstudianteRoutingModule
   ],
   declarations: [EstudianteComponent]
