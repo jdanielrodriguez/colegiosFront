5256c1e1b7dc5565ab6a869c6340f6f0616094d7
José Daniel <daniel@foxylabs.gt>
2017-09-20 01:56:21 -0600

    [UPDATED] curso-alumnos.component.ts, curso-alumnos.component.html, curso.alumnos.service.ts - initialization of curso-alumnos component for read the subjects students

--- src/app/colegio/maestro/curso-alumnos/curso-alumnos.component.ts
+++ src/app/colegio/maestro/curso-alumnos/curso-alumnos.component.ts
@@ -1,15 +1,33 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import {ActivatedRoute, Params, Router } from '@angular/router';
+import { Location } from '@angular/common';
 
+import { CursoAlumnosService } from "./../_services/curso-alumnos.service";
+import 'rxjs/add/operator/switchMap';
 @Component({
   selector: 'app-curso-alumnos',
   templateUrl: './curso-alumnos.component.html',
   styleUrls: ['./curso-alumnos.component.css']
 })
 export class CursoAlumnosComponent implements OnInit {
-
-  constructor() { }
+  @Input() id:any;
+  selectedData:any
+  constructor(
+    private route: ActivatedRoute,
+    private location:Location,
+    private router:Router,
+    private mainService:CursoAlumnosService
+  ) { }
 
   ngOnInit() {
+    this.route.params
+            .switchMap((params: Params) => this.mainService.getSingle(+params['id']))
+            .subscribe(response => { 
+                this.selectedData = response
+            });
   }
 
+  goBack(): void {
+    this.location.back();
+}
 }
