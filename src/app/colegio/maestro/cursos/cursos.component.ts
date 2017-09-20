7775f3ed40560290301a077d1b890a1ea43a48cc
José Daniel <daniel@foxylabs.gt>
2017-09-20 01:54:47 -0600

    [UPDATED] cursos.service.ts,cursos.component.html, cursos.component.ts - added cursos component chargin subijects and defined routes for each

--- src/app/colegio/maestro/cursos/cursos.component.ts
+++ src/app/colegio/maestro/cursos/cursos.component.ts
@@ -1,15 +1,64 @@
 import { Component, OnInit } from '@angular/core';
 
+import { CursosService } from "../_services/cursos.service";
+import { NotificationsService } from 'angular2-notifications';
+import { Subject } from 'rxjs/Rx';
+
 @Component({
   selector: 'app-cursos',
   templateUrl: './cursos.component.html',
   styleUrls: ['./cursos.component.css']
 })
 export class CursosComponent implements OnInit {
+  Table:any
+  selectedData:any
+  Id:any = localStorage.getItem('currentId');
+  constructor(
+    private _service: NotificationsService,
+    private mainService: CursosService
+  ) { }
+  
+    ngOnInit() {
+      this.cargarAll()
+    }
+    cargarAll(){
+      this.mainService.getAll(this.Id)
+                        .then(response => {
+                          this.Table = response
+                          console.clear 
+                        }).catch(error => {
+                          console.clear     
+                          this.createError(error) 
+                        })
+    }
+    cargarSingle(id:number){
+      this.mainService.getSingle(id)
+                        .then(response => {
+                          this.selectedData = response;
+                        }).catch(error => {
+                          console.clear     
+                          this.createError(error) 
+                        })
+    }
 
-  constructor() { }
+    
+  public options = {
+               position: ["bottom", "right"],
+               timeOut: 2000,
+               lastOnBottom: false,
+               animate: "fromLeft",  
+               showProgressBar: false,
+               pauseOnHover: true,
+               clickToClose: true,
+               maxLength: 200
+           };
+  
+    create(success) {
+                this._service.success('¡Éxito!',success)
 
-  ngOnInit() {
-  }
+    }
+    createError(error) {
+                this._service.error('¡Error!',error)
 
+    }
 }
