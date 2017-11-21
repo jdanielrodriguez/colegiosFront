6ad46ed09843bf30f0c5bc2c4b61aae78375fc89
Daniel Rodriguez <dannyjose1112@hotmail.com>
2017-11-21 15:20:13 -0600

    [UPDATED] back end nuevo

--- src/app/config.module.ts
+++ src/app/config.module.ts
@@ -1,3 +1,3 @@
 //export const path:any = { path:"http://colegios.thefoxylabs.com/" }
-//export const path:any = { path:"http://colegios.foxylabs.xyz/backend/public" }
+//export const path:any = { path:"http://backend.foxylabs.xyz/colegios/public" }
 export const path:any = { path:"http://localhost/colegios/BackEnd2/public" }
