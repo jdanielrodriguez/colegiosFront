a6006162e64fcc760986c7dbbaccbce03c60849b
Daniel Rodriguez <dannyjose1112@hotmail.com>
2017-11-21 12:08:42 -0600

    [UPDATED] config for new domain

--- src/app/config.module.ts
+++ src/app/config.module.ts
@@ -1,3 +1,3 @@
 //export const path:any = { path:"http://colegios.thefoxylabs.com/" }
-//export const path:any = { path:"http://colegiosback.foxylabs.xyz/public" }
+//export const path:any = { path:"http://colegios.foxylabs.xyz/backend/public" }
 export const path:any = { path:"http://localhost/colegios/BackEnd2/public" }
